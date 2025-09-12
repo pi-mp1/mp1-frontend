859b57c23e8b4743cf2849d2f877c9d4052e0e92
Ricardo de Jesus Zapata Cruz <ricardozapatacruz@gmail.com>
2025-09-12 09:19:59 -0500

    Add login views (#2)
    * Add login view
    
    * Create register view
    
    * Update auth layout responsive
    
    * Create reset password view

---
+++ src/layouts/index.js
@@ -0,0 +1,2 @@
+export { renderAuthLayout } from "./auth.js";
+export { renderLayout } from "./main.js";
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,5 @@
-import { TaskCard } from "../components/taskCard";
+import { renderLayout, renderAuthLayout } from "../layouts";
 import { renderTaskList } from "../components/taskList";
-import { renderLayout } from "../layouts/main";
 
 const app = document.getElementById("app");
 
@@ -16,22 +15,27 @@ export const routes = {
   home: {
     file: "home.html",
     init: initHome,
-    layout: true,
+    layout: renderLayout,
   },
   taskList: {
     file: "taskList.html",
     init: initBoard,
-    layout: true,
+    layout: renderLayout,
   },
-  login: {
+  "login": {
     file: "login.html",
     init: null,
-    layout: false,
+    layout: renderAuthLayout,
   },
-  register: {
+  "register": {
     file: "register.html",
     init: null,
-    layout: false,
+    layout: renderAuthLayout,
+  },
+  "reset-password": {
+    file: "reset-password.html",
+    init: null,
+    layout: renderAuthLayout,
   },
 };
 
@@ -50,7 +54,7 @@ export async function loadView(name) {
     const html = await res.text();
 
     // 2. Renderizar con o sin layout
-    app.innerHTML = route.layout ? renderLayout(html) : html;
+    app.innerHTML = route.layout ? route.layout(html) : html;
 
     // 3. Ejecutar inicializador (si existe)
     if (typeof route.init === "function") {
